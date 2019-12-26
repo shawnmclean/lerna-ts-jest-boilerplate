ea5bfe1dc04ac56dfb1d47f7fe509d96abc772a8
Shawn Mclean <shawnrmclean@gmail.com>
2019-12-25 20:55:15 -0500

    Add  eslint.

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "no-only-tests", "security", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:security/recommended",
+    "plugin:prettier/recommended"
+  ],
+  rules: {
+    "prettier/prettier": "error",
+    "no-only-tests/no-only-tests": "error"
+  }
+};
