7e1566e647617024bf787795885c285856a93cf9
Shawn Mclean <shawnrmclean@gmail.com>
2019-12-26 21:50:42 -0500

    Update npm packages.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,18 @@
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json"
+  },
   plugins: ["@typescript-eslint", "no-only-tests", "security", "prettier"],
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "plugin:security/recommended",
+    "prettier/@typescript-eslint",
     "plugin:prettier/recommended"
   ],
   rules: {
