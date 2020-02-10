a80552609f0d41c5213e2d1fa263ab542822a5cd
Shawn Mclean <shawnrmclean@gmail.com>
2020-02-09 21:05:04 -0500

    Cleanup question file.

--- packages/react-feedback/src/index.ts
+++ packages/react-feedback/src/index.ts
@@ -1,1 +1,2 @@
-export * from "./Questions";
+export { default as SingleChoice } from "./SingleChoice";
+export * from "./SingleChoice";
--- packages/react-feedback/src/questions/QuestionFactory.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-type Props = {};
-
-const QuestionFactory: React.FC<Props> = () => {
-  return <div></div>;
-};
-
-export default QuestionFactory;
